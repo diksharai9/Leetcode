c21484d3e55570fae2f8bed8c6391853cbc56ee1
Diksha Rai <72135805+diksharai9@users.noreply.github.com>
2022-03-28 11:38:52 +0530

    Time: 6 ms (49.66%), Space: 49.6 MB (59.15%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        ArrayList<Integer> ans = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         int n = nums.length;
-        for(int i=0; i<n; i++)
-        {
-            if(!ans.contains(nums[i]))
-            {
-                int count = 1;
-                for(int j=i+1; j<n; j++)
-                {
-                    if(nums[j]==nums[i])
-						count++;
+        int count=0;
+        for(int i=0;i<n;i++){
+            count=1;
+            if(!list.contains(nums[i])){
+                for(int j=i+1;j<n;j++){
+                    if(nums[i]==nums[j])
+                        count++;
                 }
                 if(count>n/3)
-					ans.add(nums[i]);
+                    list.add(nums[i]);
+                    
+                
             }
         }
-        return ans;
+        return list;
     }
 }
