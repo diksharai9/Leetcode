11ecd8a46bd500302f9e1d19650e396cdb86895e
Diksha Rai <72135805+diksharai9@users.noreply.github.com>
2022-05-16 10:49:36 +0530

    Time: 2 ms (38.88%), Space: 54 MB (50.38%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,20 +1,19 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         
-        boolean zeroRow = false;  //0th row pointer
-        boolean zeroCol = false; //0th col pointer
+        boolean zeroRow = false;  
+        boolean zeroCol = false; 
         
         int n = matrix.length;
         int m = matrix[0].length;
         
-        for(int i = 0 ; i < n ; i++){  //if there is a zero in oth col then zeroCol will be true means that we have make zero for 0th col elements.
+        for(int i = 0 ; i < n ; i++){  
             if(matrix[i][0] == 0){
                 zeroCol = true;
                 break;
             }
         }
         
-       // Same thing here that if there a zero present in the 0th row then all the elements here will have to be zero so make zeroRow true.
         
         for(int j = 0 ; j < m ; j++){ 
             if(matrix[0][j] == 0){
