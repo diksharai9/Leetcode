cff5ccc049083669dc60fd34b1967301f4a1fb74
Diksha Rai <72135805+diksharai9@users.noreply.github.com>
2022-07-24 00:40:31 +0530

    Time: 2 ms (59.44%), Space: 42 MB (75.06%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.java
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -1,15 +1,16 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
                 int count=0;
-        for(int i=0;i<stones.length();i++){
-            for(int j=0;j<jewels.length();j++){
-                if(stones.charAt(i)==jewels.charAt(j))
-                    count++;
-            }
-        }
-     
-        return count;
+        HashSet<Character> set=new HashSet();  
 
-        
+        for(int i=0;i<jewels.length();i++){
+            set.add(jewels.charAt(i));
+            
+        }
+        for(int i=0;i<stones.length();i++){
+            if(set.contains(stones.charAt(i)))
+                count++;
+        }
+        return count;
     }
 }
