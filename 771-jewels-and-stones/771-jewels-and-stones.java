4c9d0303c50c789df7b7186c76e6b9fcf40fbaa9
Diksha Rai <72135805+diksharai9@users.noreply.github.com>
2022-07-24 00:30:00 +0530

    Time: 2 ms (59.44%), Space: 42.9 MB (8.85%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.java
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int count=0;
+                int count=0;
         for(int i=0;i<stones.length();i++){
             for(int j=0;j<jewels.length();j++){
                 if(stones.charAt(i)==jewels.charAt(j))
@@ -9,5 +9,7 @@ class Solution {
         }
      
         return count;
+
+        
     }
 }
